45b30e3c3ea1084fb7025a4fde1a3e4c7de13ce7
loli <loli@popping>
2015-10-30 22:58:49 -0700

    heuristics

--- src/8puzzle.py
+++ src/8puzzle.py
@@ -8,6 +8,9 @@ solution = node(1, 2, 3, 4, 5, 6, 7, 8, 0, 8)
 visited = []
 
 #search takes the initial state of the problem and the search type
+#prints all expansions
+#returns false if no sol
+#returns true if sol found
 def search(initial, searchType):
 	#create a priority queue of problem states
 	nodes = PriorityQueue(initial, heuristic(initial, searchType))
@@ -18,16 +21,21 @@ def search(initial, searchType):
 			return False
 		#get lowest cost
 		node = nodes.get()
+		visited.append(node)
 		#print node and value of node
 		print("""The best state to expand with a g(n) = {} 
 			and h(n) = {} is ...""".format( 
 			heuristic(node[1], searchType),
 			node.depth)))
 		node[1].printP()
+		#solution
 		if (node == solution):
 			return True
+		#checks if node is visited already if not add to que
 		print("expanding this node ...")
-		nodes = nodes + aStar(node[1], searchType)
+		for possible in aStar(node[1], searchType):
+			if possible[1] not in visited:
+				nodes = nodes + possible
 		
 
 
--- src/search.py
+++ src/search.py
@@ -1,6 +1,24 @@
 from Queue import PriorityQueue
 from node import *
 
+#find x and y value form an 8puzzle grid
+def distance(i):
+	y = 0
+	x = 0
+	if (i <= 9 or i >= 7):
+		y = 3
+	elif (i <= 6 or i >= 4):
+		y = 2
+	else 
+		y = 1
+	if (i - 6 >= 0
+
+#used for manhattan distance
+#input is node and location of misplaced tile
+#returns misplaced distance for one piece
+def manhattan(node, j):
+	#missplaced, x and y axis of node and j
+	miss = 0
 #input is current state and witch heuristic to use
 #0 = uniforcost, 1 = misplaced tile, 2 = manhattan distance
 def heuristic(node, i):
@@ -12,7 +30,15 @@ def heuristic(node, i):
 			if node[j] != j + 1:
 				misplaced += 1
 		return misplaced
+	elfi i == 2:
+		misplaced = 0
+		for j in range(0, len(node) -2)
+			if node[j] != j + 1:
+				misplaced += manhattan(node, j)
+		return misplaced
 	else:
+		print("not 0, 1, or 2")
+		exit()
 		
 #input is state to expand and hurist value
 #returns a list of  expansions 
